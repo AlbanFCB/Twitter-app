27ac9681ded787423770bdb1b9be45609b118a11
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-03-30 21:55:39 +0200

    form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import Layout from "../components/Layout";
+import LoginModal from "../components/modals/LoginModal";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import RegisterModal from './../components/modals/RegisterModal';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Toaster/>
+      <RegisterModal/>
+      <LoginModal/>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
