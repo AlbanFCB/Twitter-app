6b63f3386b4aab4098c4d12d873b789de271a0ab
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-04-01 12:28:22 +0200

    Edit User

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import type { AppProps } from "next/app";
 import RegisterModal from './../components/modals/RegisterModal';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import EditModal from "../components/modals/EditModal";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster/>
+      <EditModal/>
       <RegisterModal/>
       <LoginModal/>
       <Layout>
