b1064b80bc34765189865d4a687c930cb56e2a77
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-04-03 12:05:52 +0200

    comment

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import Button from "./Button";
 import Avatar from './Avatar';
+import usePost from "../hooks/usePost";
 
 interface FormProps {
   placeholder: string;
@@ -19,27 +20,30 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   const loginModal = useLoginModal();
 
   const { data: currentUser } = useCurrentUser();
-  const { mutate: mutatePosts } = usePosts(postId as string);
+  const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
-  const [body, setBody] = useState("");
+  const [body, setBody] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
 
-      toast.success("Tweet created 😺");
+      await axios.post(url, { body });
 
-      setBody("");
+      toast.success('Tweet created');
+      setBody('');
       mutatePosts();
+      mutatePost();
     } catch (error) {
-      toast.error("Something went wrong!");
+      toast.error('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
